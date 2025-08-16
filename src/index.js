05ba86e94f1c6d1277b575081206cb40c33fadb3
DaemonGodx <pritam.2024ug@iiitranchi.ac.in>
2025-08-16 12:13:53 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const exp=require('express');
-const {port}=require('./config/environment')
+const {port}=require('./config/environment');
+// const {City}=require('./models/index');
+// const repo=require('./resposrity/city-repositary');
 const bodyParser=require('body-parser')
-const start= async()=> {
+const start= async ()=> {
     const app=exp();
-    app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended:true}));
+    
+        app.listen(port, ()=>{
+   
+        console.log(`Server is running on port ${port}`);
+        // City.create({
+        //     name:"delhi",
+        // })
+    //    const  repox=new repo();
+    //     repox.deletecity(1);
+   
 });
 };
 start();
---
+++ src/resposrity/city-repositary.js
@@ -0,0 +1,33 @@
+const {City}=require('../models/index');
+
+class CreateRepo{
+    async  createcity({name})
+    {
+        try
+        {
+            const city= await City.create({name});
+            return city;
+        }
+        catch(error)
+        {
+            throw {error};
+        }
+    }
+     async  deletecity(cityid)
+    {
+        try
+        {
+         await City.destroy({
+            where :{
+                id:cityid
+            }
+        });
+            
+        }
+        catch(error)
+        {
+            throw {error};
+        }
+    }
+}
+module.exports=CreateRepo;
